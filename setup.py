9acf95734378254ff9c40fcb4121423da5996d21
Pavel Golovatenko-Abramov <p.golovatenko@gmail.com>
2020-06-12 13:08:19 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ except ModuleNotFoundError:
 finally:
     setup(
         name='sic',
-        version='1.0.0',
+        version='1.0.1',
         description='Utility for string normalization',
         url='https://github.com/pgolo/sic',
         author='Pavel Golovatenko-Abramov',
         author_email='p.golovatenko@gmail.com',
         packages=['sic'],
         ext_modules=ext_modules,
-        package_data={'sic': ['tokenizer.greek.xml', 'tokenizer.standard.xml', 'tokenizer.western.xml']},
+        package_data={'sic': ['core.c', 'core.pxd', 'tokenizer.greek.xml', 'tokenizer.standard.xml', 'tokenizer.western.xml']},
         include_package_data=True,
         license='MIT',
         platforms=['any'],
