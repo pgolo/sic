75e47789e5bed13d71a09fcf81b8e3aabae947ab
Pavel Golovatenko-Abramov <p.golovatenko@gmail.com>
2020-06-13 01:20:41 -0400

    indicated support for Python 3.6, 3.7, 3.8

--- setup.py
+++ setup.py
@@ -25,7 +25,9 @@ finally:
         license='MIT',
         platforms=['any'],
         classifiers=[
-            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
             'License :: OSI Approved :: MIT License',
             'Operating System :: OS Independent',
         ],
