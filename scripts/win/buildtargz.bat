bcd053c35da1fcebbbbe8f0d2a6449ef3b524616
pgolo <p.golovatenko@gmail.com>
2020-09-02 15:13:38 -0400

    different scripts for sdist and bdist

--- scripts/win/buildtargz.bat
+++ scripts/win/buildtargz.bat
@@ -1,6 +1,6 @@
 @echo off
 rem Usage:
-rem builddist.bat path\to\python36 path\to\python37 path\to\python38
+rem buildtargz.bat path\to\python
 
 set RUNDIR=%cd%
 set MYDIR=%~dp0
@@ -8,13 +8,10 @@ set ROOT=%MYDIR%\..\..
 set REQUIREMENTS=%ROOT%\requirements.txt
 set ENV=%ROOT%\.env.build
 
-:BUILD
 if (%1)==() (exit)
-if not exist "%1" (echo "%1": Python not found && shift && goto BUILD)
+if not exist "%1" (echo "%1": Python not found && exit)
 cd "%ROOT%"
 virtualenv -p "%1" "%ENV%"
 "%ENV%"\Scripts\python -m pip install --no-cache-dir -r "%REQUIREMENTS%"
-"%ENV%"\Scripts\python "%ROOT%"\setup.py bdist_wheel
+"%ENV%"\Scripts\python "%ROOT%"\setup.py sdist
 rmdir /S /Q "%ENV%"
-shift
-goto BUILD
