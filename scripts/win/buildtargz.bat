606ed01d4f036a8cf5e5b14ec1f3b613d48ab099
pgolo <p.golovatenko@gmail.com>
2020-09-02 15:38:38 -0400

    adjustments in building scripts

--- scripts/win/buildtargz.bat
+++ scripts/win/buildtargz.bat
@@ -8,10 +8,12 @@ set ROOT=%MYDIR%\..\..
 set REQUIREMENTS=%ROOT%\requirements.txt
 set ENV=%ROOT%\.env.build
 
-if (%1)==() (exit)
+if (%1)==() (cd %RUNDIR% && exit)
 if not exist "%1" (echo "%1": Python not found && exit)
 cd "%ROOT%"
 virtualenv -p "%1" "%ENV%"
 "%ENV%"\Scripts\python -m pip install --no-cache-dir -r "%REQUIREMENTS%"
 "%ENV%"\Scripts\python "%ROOT%"\setup.py sdist
 rmdir /S /Q "%ENV%"
+
+cd %RUNDIR%
