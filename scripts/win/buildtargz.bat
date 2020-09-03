59c23b33fff2b6389d8529afb16b35e20af790b1
pgolo <p.golovatenko@gmail.com>
2020-09-03 01:04:18 -0400

    chdir back to running directory

--- scripts/win/buildtargz.bat
+++ scripts/win/buildtargz.bat
@@ -5,12 +5,11 @@ rem buildtargz.bat path\to\python
 set RUNDIR=%cd%
 set MYDIR=%~dp0
 set ROOT=%MYDIR%\..\..
-set REQUIREMENTS=%ROOT%\requirements.txt
 set ENV=%ROOT%\.env.build
 set SHIPPING=%ROOT%\shipping
 
 if (%1)==() (cd %RUNDIR% && exit)
-if not exist "%1" (echo "%1": Python not found && exit)
+if not exist "%1" (echo "%1": Python not found && cd %RUNDIR% && exit)
 cd "%ROOT%"
 virtualenv -p "%1" "%ENV%"
 "%ENV%"\Scripts\python "%SHIPPING%"\make_setup.py sdist
