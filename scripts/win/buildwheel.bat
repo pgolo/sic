f8adea52daee0012e12624ed4ebc0831d70d2a92
pgolo <p.golovatenko@gmail.com>
2020-09-02 21:49:41 -0400

    wrkarnd for different setup.py for sdist and wheel

--- scripts/win/buildwheel.bat
+++ scripts/win/buildwheel.bat
@@ -7,13 +7,15 @@ set MYDIR=%~dp0
 set ROOT=%MYDIR%\..\..
 set REQUIREMENTS=%ROOT%\requirements.txt
 set ENV=%ROOT%\.env.build
+set SHIPPING=%ROOT%\shipping
 
 :BUILD
-if (%1)==() (cd %RUNDIR% && exit)
+if (%1)==() (goto FINISH)
 if not exist "%1" (echo "%1": Python not found && shift && goto BUILD)
 cd "%ROOT%"
 virtualenv -p "%1" "%ENV%"
 "%ENV%"\Scripts\python -m pip install --no-cache-dir -r "%REQUIREMENTS%"
+"%ENV%"\Scripts\python "%SHIPPING%"\make_setup.py bdist_wheel
 "%ENV%"\Scripts\python "%ROOT%"\setup.py bdist_wheel
 rmdir /S /Q "%ENV%"
 rmdir /S /Q "%ROOT%"\sic.egg-info
@@ -22,4 +24,6 @@ del /Q "%ROOT%"\sic\core.c
 shift
 goto BUILD
 
+:FINISH
+del /Q "%ROOT%"\setup.py
 cd %RUNDIR%
