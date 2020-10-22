61936c9734dcebf86be2585ee6c08c85887ec9ae
pgolo <p.golovatenko@gmail.com>
2020-10-21 23:58:30 -0400

    ad hoc normalization

--- sic/implicit.pxd
+++ sic/implicit.pxd
@@ -6,11 +6,4 @@ cpdef build_normalizer(
     str filename=*
 )
 
-@cython.locals(
-    result=cython.str
-)
-cpdef str normalize(
-    str source_string,
-    str word_separator=*,
-    int normalizer_option=*
-)
+cpdef reset()
--- sic/implicit.py
+++ sic/implicit.py
@@ -7,9 +7,21 @@ def build_normalizer(filename=None):
     __builder__ = Builder()
     __normalizer__ = __builder__.build_normalizer(filename)
 
-def normalize(source_string, word_separator=' ', normalizer_option=0):
+def normalize(*args, **kwargs):
+    kwargs['source_string'] = args[0] if len(args) > 0 else kwargs['source_string'] if 'source_string' in kwargs else ''
+    kwargs['word_separator'] = args[1] if len(args) > 1 else kwargs['word_separator'] if 'word_separator' in kwargs else ' '
+    kwargs['normalizer_option'] = args[2] if len(args) > 2 else kwargs['normalizer_option'] if 'normalizer_option' in kwargs else 0
+    if 'tokenizer_config' in kwargs:
+        builder = Builder()
+        normalizer = builder.build_normalizer(kwargs['tokenizer_config'])
+        result = normalizer.normalize(kwargs['source_string'], kwargs['word_separator'], kwargs['normalizer_option'])
+        return result
     global __normalizer__
     if not  __normalizer__:
         build_normalizer()
-    result = __normalizer__.normalize(source_string, word_separator, normalizer_option)
+    result = __normalizer__.normalize(kwargs['source_string'], kwargs['word_separator'], kwargs['normalizer_option'])
     return result
+
+def reset():
+    global __normalizer__
+    __normalizer__ = None
