0c59298edfb5dd6feec531e93e81c0ded4409d18
pgolo <p.golovatenko@gmail.com>
2020-11-09 11:14:12 -0500

    flushed version

--- shipping/setup.py
+++ shipping/setup.py
@@ -9,7 +9,7 @@ with open('README.md', mode='r', encoding='utf8') as f:
 
 setup(
     name='sic',
-    version='1.2.0',
+    version='X.X.X',
     description='Utility for string normalization',
     long_description=long_description,
     long_description_content_type='text/markdown',
